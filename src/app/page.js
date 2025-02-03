33c390cdf2d41cf6de809c3ee134cb27f0007957
clement <clementadebola54@gmail.com>
2025-02-03 15:12:14 +0100

    new

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,34 @@ export default function Home() {
     const yValues = [100, -150, -400];
 
 
-     
+    gsap.utils.toArray(".row").forEach((row, index) =>{
+      const cardleft = row.querySelector(".card-left");
+      const cardRight = row.querySelector(".card-right");
+
+      gsap.to(cardleft, {
+        x: leftXValues[index],
+        scrollTrigger: {
+          trigger: ".main",
+          start: "top center",
+          end: "150% bottom",
+          scrub: true,
+
+          onUpdate: (self) => {
+            const progress = self.progress;
+            cardleft.style.transform = `translateX(${
+              progress * leftXValues[index]
+            }px translateY${progress * yValues[index]}px rotate(${progress * leftRotationValues[index]}deg)`;
+
+            cardRight.style.transform = `translateX(${
+              progress * rightXValues[index]
+            }px translateY${progress * yValues[index]}px rotate(${progress * rightRotationValues[index]}deg)`;
+          }
+        }
+      })
+
+      gsap.to
+    }); 
+
   }, []);
 
 
