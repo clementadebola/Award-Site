72ad386567ed78e10dc968a50aa40ca5f76de466
clement <clementadebola54@gmail.com>
2025-02-03 15:53:06 +0100

    new

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import { ReactLenis } from "lenis/react";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-  useEffect(() =>{
+  useEffect(() => {
     const ScrollTriggerSettings = {
       trigger: ".main",
       start: "top 25%",
@@ -21,8 +21,7 @@ export default function Home() {
     const rightRotationValues = [30, 20, 35];
     const yValues = [100, -150, -400];
 
-
-    gsap.utils.toArray(".row").forEach((row, index) =>{
+    gsap.utils.toArray(".row").forEach((row, index) => {
       const cardleft = row.querySelector(".card-left");
       const cardRight = row.querySelector(".card-right");
 
@@ -38,34 +37,37 @@ export default function Home() {
             const progress = self.progress;
             cardleft.style.transform = `translateX(${
               progress * leftXValues[index]
-            }px translateY${progress * yValues[index]}px rotate(${progress * leftRotationValues[index]}deg)`;
+            }px) translateY(${progress * yValues[index]}px) rotate(${
+              progress * leftRotationValues[index]
+            }deg)`;
 
+            
             cardRight.style.transform = `translateX(${
               progress * rightXValues[index]
-            }px translateY${progress * yValues[index]}px rotate(${progress * rightRotationValues[index]}deg)`;
-          }
-        }
-      })
-    }); 
+            }px) translateY(${progress * yValues[index]}px) rotate(${
+              progress * rightRotationValues[index]
+            }deg)`;
+          },
+        },
+      });
+    });
 
-
-    gsap.to(".logo",{
-      scale: 1,
-      duration: 0.5,
-      ease: "power1.out",
-      scrollTrigger: ScrollTriggerSettings,
-    })
-
-    gsap.to(".line p",{
-      y: 0,
-      stagger: 0.1,
+    gsap.to(".logo", {
       scale: 1,
       duration: 0.5,
       ease: "power1.out",
       scrollTrigger: ScrollTriggerSettings,
     });
 
-    gsap.to("button",{
+    gsap.to(".line p", {
+      y: 0,
+      stagger: 0.1,
+      duration: 0.5,
+      ease: "power1.out",
+      scrollTrigger: ScrollTriggerSettings,
+    });
+
+    gsap.to("button", {
       y: 0,
       opacity: 1,
       delay: 0.25,
@@ -74,24 +76,21 @@ export default function Home() {
       scrollTrigger: ScrollTriggerSettings,
     });
 
-
     return () => {
-      ScrollTrigger.getAll().forEach((trigger) => trigger.kill())
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
-
   }, []);
-
 
   const generateRows = () => {
     const rows = [];
-    for (let i = 1; i <=3; i++){
+    for (let i = 1; i <= 3; i++) {
       rows.push(
         <div className="row" key={i}>
           <div className="card card-left">
             <img src={`/img-${2 * i - 1}.jpg`} alt=""></img>
           </div>
           <div className="card card-right">
-          <img src={`/img-${2 * i}.jpg`} alt=""></img>
+            <img src={`/img-${2 * i}.jpg`} alt=""></img>
           </div>
         </div>
       );
@@ -109,32 +108,28 @@ export default function Home() {
         </section>
         <section className="main">
           <div className="main-content">
-          <div className="logo">
-            <img src="/logo.jpg" />
-          </div>
-          <div className="copy">
-            <div className="line">
-              <p>Delve into coding without clutter.</p>
+            <div className="logo">
+              <img src="/logo.jpg" />
             </div>
-            <div className="line">
-              <p>One subscription. Endless web design.</p>
+            <div className="copy">
+              <div className="line">
+                <p>Delve into coding without clutter.</p>
+              </div>
+              <div className="line">
+                <p>One subscription. Endless web design.</p>
+              </div>
+              <div className="line">
+                <p>Take the fast lane to mastery.</p>
+              </div>
             </div>
-            <div className="line">
-              <p>Take the fast lane to mastery.</p>
-            </div>
-
-          </div>
-          <div className="btn">
+            <div className="btn">
               <button>Get Pro</button>
             </div>
-
-         
           </div>
           {generateRows()}
         </section>
         <section className="footer">
           <Link href="#">Link in description</Link>
-
         </section>
       </ReactLenis>
     </>
